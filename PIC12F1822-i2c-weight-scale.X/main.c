e49dc695b2d6afe0b21dc1fee9b291b7c19546bd
Simon Lundmark <simon.lundmark@gmail.com>
2014-11-03 22:26:34 +0100

    Hello, world (blinking LED on pin 2)

---
+++ PIC12F1822-i2c-weight-scale.X/main.c
@@ -0,0 +1,44 @@
+#include <xc.h>
+
+// PIC12F1822 Configuration Bit Settings
+
+// CONFIG1
+#pragma config FOSC = INTOSC    // Oscillator Selection (INTOSC oscillator: I/O function on CLKIN pin)
+#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
+#pragma config PWRTE = ON       // Power-up Timer Enable (PWRT enabled)
+#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Memory Code Protection (Data memory code protection is disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
+#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+#pragma config IESO = OFF       // Internal/External Switchover (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is disabled)
+
+// CONFIG2
+#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+#pragma config PLLEN = ON       // PLL Enable (4x PLL enabled)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+#pragma config BORV = HI        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), high trip point selected.)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable (High-voltage on MCLR/VPP must be used for programming)
+
+// #pragma config statements should precede project file includes.
+#include <stdlib.h>
+
+
+#define _XTAL_FREQ 500000
+
+
+int main(void) {
+    // Set up data direction
+    TRISA5 = 0;
+
+    // Write to an output pin
+    while (1) {
+        PORTAbits.RA5 = 1;
+        __delay_ms(1000);
+        PORTAbits.RA5 = 0;
+        __delay_ms(1000);
+    }
+
+    return (EXIT_SUCCESS);
+}
