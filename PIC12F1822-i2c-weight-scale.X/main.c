7169b90a15e24ac4de749dea346f9c1cf175f3f9
Simon Lundmark <simon.lundmark@gmail.com>
2014-11-06 00:46:11 +0100

    WIP: Started implementing an I2C slave
    - Currently an Electric Imp I2C master can successfully read one byte!

--- PIC12F1822-i2c-weight-scale.X/main.c
+++ PIC12F1822-i2c-weight-scale.X/main.c
@@ -41,20 +41,99 @@ void configure_oscillator(void) {
 }
 
 
-void blink(void) {
+void configure_io(void) {
     // Set A5 pin as an output
     TRISA5 = 0;
+}
 
-    while (1) {
-        PORTAbits.RA5 = 1;
-        __delay_ms(1000);
-        PORTAbits.RA5 = 0;
-        __delay_ms(1000);
-    }
+
+void blink_once(void) {
+    PORTAbits.RA5 = 1;
+    __delay_ms(500);
+    PORTAbits.RA5 = 0;
+    __delay_ms(500);
+}
+
+
+void configure_i2c(void) {
+    // Set SCL and SDA pins as inputs by settings the TRIS bits (p. 229)
+    TRISA1 = 1; // SCL
+    TRISA2 = 1; // SDA
+
+    // Setup MSSP as I2C slave mode, 7-bit address
+    // with Start and Stop bit interrupts
+    SSPCON1bits.SSPM = 0b1110;
+
+    // Set I2C slave address to decimal 17
+    SSP1ADDbits.SSPADD = 0b00100010; // Shifted 1 bit to the left
+    
+    // Enable the MSSP module
+    SSPCON1bits.SSPEN = 1;
+
+    // Enable the MSSP interrupt
+    SSP1IE = 1;
+    
+    // Enable all active peripheral interrupts
+    PEIE = 1;
+
+    // Enable all active interrupts
+    GIE = 1;
 }
 
 
 int main(void) {
     configure_oscillator();
-    blink();
+    configure_io();
+
+    blink_once();
+
+    configure_i2c();
+
+    blink_once();
+
+    while (1);
+}
+
+
+typedef unsigned char bool;
+#define true    1
+#define false   0
+
+bool is_i2c_write_operation_and_got_address_byte(void) {
+    return false; // TODO
+}
+
+bool is_i2c_write_operation_and_got_data_byte(void) {
+    return false; // TODO
+}
+
+bool is_i2c_read_operation_and_got_address_byte(void) {
+    // S = 1, D_A = 0, R_W = 1
+    if (SSP1STATbits.S == 1 && SSP1STATbits.D_nA == 0 && SSP1STATbits.R_nW == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void interrupt ISR(void) {
+    if (SSP1IF == 1) {
+        SSP1IF = 0;
+        
+        // Read address
+        if (is_i2c_write_operation_and_got_address_byte()) {
+
+        } else if (is_i2c_write_operation_and_got_data_byte()) {
+
+        } else if (is_i2c_read_operation_and_got_address_byte()) {
+            unsigned char buffer;
+            buffer = SSP1BUF;
+
+            SSP1BUF = 'a';
+
+            SSP1CON1bits.CKP = 1;
+            
+            blink_once();
+        }
+    }
 }
