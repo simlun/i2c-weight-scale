c756a46f13e71b57bc5e10a32066dbe8cc078ae2
Simon Lundmark <simon.lundmark@gmail.com>
2014-11-09 20:17:39 +0100

    Refactoring and improved I2C implementation

--- PIC12F1822-i2c-weight-scale.X/main.c
+++ PIC12F1822-i2c-weight-scale.X/main.c
@@ -48,7 +48,7 @@ void initialize_data_variables(void) {
 }
 
 
-void configure_oscillator(void) {
+void initialize_oscillator(void) {
     // Set internal oscillator block
     OSCCONbits.SCS = 0b10;
 
@@ -60,7 +60,7 @@ void configure_oscillator(void) {
 }
 
 
-void configure_io(void) {
+void initialize_io(void) {
     // Set A5 pin as an output
     TRISA5 = 0;
 }
@@ -68,7 +68,7 @@ void configure_io(void) {
 
 // ----------------------------------------------------------------------
 // I2C implementation
-void configure_i2c(void) {
+void initialize_i2c_readonly_slave(uint8_t seven_bit_address) {
     // Set SCL and SDA pins as inputs by settings the TRIS bits (p. 229)
     TRISA1 = 1; // SCL
     TRISA2 = 1; // SDA
@@ -77,8 +77,8 @@ void configure_i2c(void) {
     // with Start and Stop bit interrupts
     SSPCON1bits.SSPM = 0b1110;
 
-    // Set I2C slave address to decimal 17
-    SSP1ADDbits.SSPADD = 0b00100010; // Shifted 1 bit to the left
+    // Set I2C slave address
+    SSP1ADDbits.SSPADD = seven_bit_address << 1;
 
     // Enable the MSSP module
     SSPCON1bits.SSPEN = 1;
@@ -94,14 +94,18 @@ void configure_i2c(void) {
 }
 
 bool is_i2c_read_operation_and_last_bit_was_an_address_byte(void) {
+    // "State 3" from the Microchip application notes AN734
     // S = 1, D_A = 0, R_W = 1
     return (SSP1STATbits.S == 1 && SSP1STATbits.D_nA == 0 && SSP1STATbits.R_nW == 1);
 }
 
 bool is_i2c_read_operation_and_last_byte_was_a_data_byte(void) {
+    // "State 4" from the Microchip application notes AN734
     // S = 1, D_A = 1, R_W = 1, BF = 0
     return (SSP1STATbits.S == 1 && SSP1STATbits.D_nA == 1 && SSP1STATbits.R_nW == 1 && SSP1STATbits.BF == 0);
 }
+
+volatile static bit i2c_buffer_index = 0;
 
 /*
  * Take a 16 bit source value and return the first 8 bits the first time this
@@ -112,10 +116,9 @@ bool is_i2c_read_operation_and_last_byte_was_a_data_byte(void) {
  * Useful for sending an uint16_t over I2C in two subsequent 8-bit writes.
  */
 uint8_t chunked(uint16_t source) {
-    static bit index = 0;
-    static uint16_t my_source;
+    volatile static uint16_t my_source;
 
-    if (index == 0) {
+    if (i2c_buffer_index == 0) {
         my_source = source;
     }
 
@@ -123,11 +126,43 @@ uint8_t chunked(uint16_t source) {
     chunks[0] = my_source >> 8;
     chunks[1] = (my_source << 8) >> 8;
 
-    uint8_t return_value = chunks[index];
+    uint8_t return_value = chunks[i2c_buffer_index];
 
-    index = !index;
+    i2c_buffer_index = !i2c_buffer_index;
 
     return return_value;
+}
+
+void i2c_write(uint8_t data) {
+    // The "WriteI2C" subroutine from the Microchip application notes AN734
+    // converted into C code
+
+    // Wait for buffer to be empty
+    while (SSP1STATbits.BF == 1);
+
+    // Write data
+    SSP1CON1bits.WCOL = 0;
+    do {
+        SSP1BUF = data;
+    } while (SSP1CON1bits.WCOL == 1);
+
+    // Release the clock, we are done
+    SSP1CON1bits.CKP = 1;
+}
+
+void i2c_2_byte_master_read_request_handler(uint16_t * data_to_send_to_master) {
+    if (SSP1IF == 1) {
+        SSP1IF = 0;
+
+        if (is_i2c_read_operation_and_last_bit_was_an_address_byte()) {
+            // Read address from buffer, clearing BF
+            unsigned char tmp = SSP1BUF;
+            i2c_buffer_index = 0;
+            i2c_write(chunked(*data_to_send_to_master));
+        } else if (is_i2c_read_operation_and_last_byte_was_a_data_byte()) {
+            i2c_write(chunked(*data_to_send_to_master));
+        }
+    }
 }
 
 
@@ -144,30 +179,22 @@ void blink_once(void) {
 // ----------------------------------------------------------------------
 // Main
 int main(void) {
-    configure_oscillator();
+    initialize_oscillator();
     initialize_data_variables();
-    configure_io();
+    initialize_io();
+    initialize_i2c_readonly_slave(0x11);
 
     blink_once();
 
-    configure_i2c();
-
-    blink_once();
-
-    while (1);
+    while (1) {
+        __delay_ms(1000);
+        foo++;
+    }
 }
 
 
 // ----------------------------------------------------------------------
 // Interrupt Service Routine
 void interrupt ISR(void) {
-    if (SSP1IF == 1) {
-        SSP1IF = 0;
-
-        if (is_i2c_read_operation_and_last_bit_was_an_address_byte()
-                || is_i2c_read_operation_and_last_byte_was_a_data_byte()) {
-            SSP1BUF = chunked(foo);
-            SSP1CON1bits.CKP = 1;
-        }
-    }
+    i2c_2_byte_master_read_request_handler(&foo);
 }
